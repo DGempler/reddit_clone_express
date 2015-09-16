3df8f547636d9730bfcb4f35a3bd054709a7a64f
Dmitry Gempler <DGempler@gmail.com>
2015-09-16 14:06:38 -0700

    updated app.js

--- app.js
+++ app.js
@@ -4,20 +4,18 @@ var bodyParser = require('body-parser');
 var db = require('./models');
 var exphbs = require('express-handlebars');
 var morgan = require("morgan");
-var session = require('cookie-session')
+var session = require('cookie-session');
 
-
-​
 //creates global variable
 app = express();
-​
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-​
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
-​
+
 app.use(session({
   maxAge: 3600000,
   secret: "specialspecial",
@@ -25,13 +23,6 @@ app.use(session({
 }));
 
 require('./controllers/index');
-​
-
-
-
-
-
-
 
 app.listen(3000, function() {
   console.log("Starting a server on http://localhost:3000");
