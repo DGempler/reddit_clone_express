82fce06d29f691aa877a9c87e0971bcd773bd272
Dmitry Gempler <DGempler@gmail.com>
2015-09-16 11:57:26 -0700

    updated user model again with pre-remove middleware

--- models/user.js
+++ models/user.js
@@ -47,6 +47,14 @@ userSchema.pre('save', function(next) {
   });
 });
 
+userSchema.pre('remove', function(next) {
+  Post.remove({user_id: this._id}).exec();
+  Comment.remove({user_id: this._id}).exec();
+  next();
+});
+
+
+
 userSchema.statics.authenticate = function (formData, callback) {
   this.findOne({
     email: formData.email
