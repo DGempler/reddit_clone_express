ddbaf53bd535d5b712ebf70cdaf2b454ebfff1a6
LionelLints <lionel.lints@gmail.com>
2015-09-16 12:39:23 -0700

    update post model

---
+++ models/post.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+
+var postSchema = new mongoose.Schema({
+  title: String,
+  body: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Comment"
+  }]
+});
+
+
+var Post = mongoose.model("Post", userSchema);
+
+module.exports = Post;
