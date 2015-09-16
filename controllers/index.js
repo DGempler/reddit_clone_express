1b4c845601c27dd78065b6afde96b8b33176777e
LionelLints <lionel.lints@gmail.com>
2015-09-16 13:10:02 -0700

    update loginhelper

--- controllers/index.js
+++ controllers/index.js
@@ -2,3 +2,10 @@ app.get('/', function (req, res){
   res.render('/index');
 });
 
+var loginMiddleware = require('../middleware/loginHelper.js');
+
+app.use(loginMiddleware);
+
+require('./users');
+require('./posts');
+require('./comments');
